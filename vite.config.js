d05c76c48c9b391953572393cfcbdb67cc3e58cd
Akshay Jumbade <akkyjumbade@gmail.com>
2025-01-08 18:02:14 +0530

    updated admin panel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: [
+                'resources/js/app.tsx',
+                'laravel-cp/frontend/main.ts',
+                // 'laravel-cp/frontend/app.tsx',
+            ],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
         react(),
+        svelte({
+            /* plugin options */
+        })
     ],
 });
